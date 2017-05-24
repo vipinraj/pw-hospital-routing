42e1788e694547ada6bc5afb0a5fec1a57809b84
Vipin Raj <vipin.mscis3@iiitmk.ac.in>
2017-05-24 13:44:37 +0530

    Update beacon-latlong-characteristic.js

--- Learning/location-beacon/beacon-latlong-characteristic.js
+++ Learning/location-beacon/beacon-latlong-characteristic.js
@@ -5,7 +5,7 @@ function LatLongCharacteristic() {
   bleno.Characteristic.call(this, {
     uuid: '13333333333333333333333333330002',
     properties: ['read'],
-    value: new Buffer("3242.877:4556.6655"),
+    value: new Buffer("12.972442:77.580643"),
     descriptors: [
       new bleno.Descriptor({
         uuid: '2901',
