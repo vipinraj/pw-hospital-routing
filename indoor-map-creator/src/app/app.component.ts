aeacabb57609cfe23106f94dee0d2804550c8975
Vipin <kpvipinraj@gmail.com>
2017-08-18 08:33:50 +0530

    my account -> menu

--- indoor-map-creator/src/app/app.component.ts
+++ indoor-map-creator/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { MapComponent } from './map/map.component';
 import { SidePaneComponent } from './side-pane/side-pane.component';
 import { GoogleMapsAPIWrapper } from '@agm/core';
 import { MapApiService } from './services/map-api.service';
 import { MdDialog } from '@angular/material';
 import { UserAccountComponent } from './user-account/user-account.component';
+declare var gapi: any;
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ import { UserAccountComponent } from './user-account/user-account.component';
   styleUrls: ['./app.component.css'],
   providers: [GoogleMapsAPIWrapper, MapApiService]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   mapApi: GoogleMapsAPIWrapper;
   constructor(private _mapApi: GoogleMapsAPIWrapper, private _internalMapApiService: MapApiService, public dialog: MdDialog) {
     this.mapApi = _mapApi;
@@ -20,6 +21,14 @@ export class AppComponent {
   }
 
   onMyAccountClick() {
-    this.dialog.open(UserAccountComponent, { disableClose: false, width: '60vw', height: '50vh'});
+    this.dialog.open(UserAccountComponent, { disableClose: false, width: '60vw', height: '57vh' });
+  }
+
+  onLogoutClick() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut();
+  }
+  ngAfterViewInit() {
+    var auth2 = gapi.auth2.getAuthInstance();
   }
 }
